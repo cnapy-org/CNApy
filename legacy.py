dc12e0c40b75b82b01ebf2f7be7416a97226b464
Sven Thiele <sthiele78@gmail.com>
2020-05-28 10:37:51 +0200

    connect to legacy CNA (#7)

--- legacy.py
+++ legacy.py
@@ -1,14 +1,43 @@
+import cobra
+import io
 try:
     import matlab.engine
     eng = matlab.engine.start_matlab()
+
+    out = io.StringIO()
+    err = io.StringIO()
+    print("Matlab engine found")
     me = True
 except:
     print("Matlab engine not found")
     me = False
 
 
-def matlabcall():
+CNA_PATH = "/scratch/CellNetAnalyzer/"
+
+
+def createCobraModel(model):
     if me:
-        tf = eng.isprime(37)
-        print(tf)
-        return tf
+        a = eng.eval('cd("'+CNA_PATH+'")')
+        cobra.io.save_matlab_model(
+            model, CNA_PATH+"cobra_model.mat", varname="cbmodel")
+
+
+def legacy_function(model):
+    if me:
+        createCobraModel(model)
+        print(".")
+        a = eng.eval("startcna(1)", nargout=0, stdout=out, stderr=err)
+        print(".")
+        a = eng.eval("load('cobra_model.mat')",
+                     nargout=0, stdout=out, stderr=err)
+        print(".")
+        a = eng.eval("cnap =CNAcobra2cna(cbmodel);",
+                     nargout=0, stdout=out, stderr=err)
+        print(".")
+        a = eng.eval(
+            "[ems, irrev_ems, ems_idx] = CNAcomputeEFM(cnap);", nargout=0, stdout=out, stderr=err)
+        ems = eng.workspace['ems']
+        print(ems)
+        print("ems length:", str(len(ems)))
+    return
