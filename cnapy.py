0e9eefe4564d079573f3e38180dc7be974766da4
Sven Thiele <sthiele78@gmail.com>
2020-09-16 13:28:51 +0200

    cleanup

--- cnapy.py
+++ cnapy.py
@@ -12,7 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# from cnapy.cellnetanalyzer import CellNetAnalyzer
+# limitations under the License.
 from cnapy.cellnetanalyzer import CellNetAnalyzer
 
 cna = CellNetAnalyzer()
