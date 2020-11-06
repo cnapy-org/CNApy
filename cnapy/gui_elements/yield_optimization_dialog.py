a6b66d009c2204d821efdf1f6caff42b3b2093e7
Sven Thiele <sthiele78@gmail.com>
2020-11-06 11:04:48 +0100

    add validator

--- cnapy/gui_elements/yield_optimization_dialog.py
+++ cnapy/gui_elements/yield_optimization_dialog.py
@@ -9,14 +9,14 @@ from matplotlib.pyplot import scatter
 from qtpy.QtCore import Qt, Signal
 from qtpy.QtWidgets import (QCompleter, QDialog, QHBoxLayout, QLabel,
                             QLineEdit, QMessageBox, QPushButton, QVBoxLayout)
-
+import re
 import cnapy.legacy as legacy
 from cnapy.cnadata import CnaData
 from cnapy.legacy import get_matlab_engine
 
 
 class CompleterLineEdit(QLineEdit):
-    # does new completion after COMMA ,
+    # does new completion after Plussig '+'
 
     def __init__(self, wordlist, *args):
         QLineEdit.__init__(self, *args)
@@ -29,8 +29,8 @@ class CompleterLineEdit(QLineEdit):
     def text_changed(self, text):
         all_text = text
         text = all_text[:self.cursorPosition()]
-        prefix = text.split(',')[-1].strip()
-
+        prefix = text.split('+')[-1].strip()
+        prefix = prefix.split(' ')[-1].strip()
         self.mycompleter.setCompletionPrefix(prefix)
         if prefix != '':
             self.mycompleter.complete()
@@ -39,7 +39,9 @@ class CompleterLineEdit(QLineEdit):
         cursor_pos = self.cursorPosition()
         before_text = self.text()[:cursor_pos]
         after_text = self.text()[cursor_pos:]
-        prefix_len = len(before_text.split(',')[-1].strip())
+        prefix = before_text.split('+')[-1]
+        prefix = prefix.split(' ')[-1]
+        prefix_len = len(prefix.strip())
         self.setText(before_text[:cursor_pos - prefix_len] + text + after_text)
         self.setCursorPosition(cursor_pos - prefix_len + len(text))
 
@@ -55,6 +57,8 @@ class YieldOptimizationDialog(QDialog):
         self.appdata = appdata
         self.eng = get_matlab_engine()
 
+        self.polynom_re = re.compile(
+            '([ ]*(?P<factor>\d*)[ ]*[*]?[ ]*(?P<reac_id>[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW]+\w*)[ ]*)')
         completer = QCompleter(
             self.appdata.project.cobra_py_model.reactions.list_attr("id"), self)
         completer.setCaseSensitivity(Qt.CaseInsensitive)
@@ -70,7 +74,8 @@ class YieldOptimizationDialog(QDialog):
         t2 = QLabel(
             "Define d of the yield function Y=c*r/d*r by providing relevant reaction identifiers (each optionally followed by a weighting factor)")
         self.layout.addWidget(t2)
-        self.d = QLineEdit("")
+        self.d = CompleterLineEdit(
+            self.appdata.project.cobra_py_model.reactions.list_attr("id"), "")
         self.d.setPlaceholderText("")
         self.d.setCompleter(completer)
         self.layout.addWidget(self.d)
@@ -84,8 +89,67 @@ class YieldOptimizationDialog(QDialog):
         self.setLayout(self.layout)
 
         # Connecting the signal
+        self.c.textChanged.connect(self.validate_dialog)
+        self.d.textChanged.connect(self.validate_dialog)
         self.cancel.clicked.connect(self.reject)
         self.button.clicked.connect(self.compute)
+
+        self.validate_dialog()
+
+    def validate_dialog(self):
+
+        valid_c = self.validate_c()
+        valid_d = self.validate_d()
+        if valid_c and valid_d:
+            self.button.setEnabled(True)
+        else:
+            self.button.setEnabled(False)
+
+    def validate_polynom(self, text: str):
+        texts = text.split('+')
+        valid = True
+        for elem in texts:
+            match = self.polynom_re.fullmatch(elem)
+            print(match)
+            if match == None:
+                valid = False
+            else:
+                print('fak:', match.groupdict()['factor'])
+                print('reac_id:', match.groupdict()['reac_id'])
+                if match.groupdict()['reac_id'] not in self.appdata.project.cobra_py_model.reactions.list_attr("id"):
+                    valid = False
+        return valid
+
+    def validate_c(self):
+
+        palette = self.c.palette()
+        role = self.c.foregroundRole()
+        palette.setColor(role, Qt.black)
+        self.c.setPalette(palette)
+
+        text = self.c.text()
+        valid = self.validate_polynom(text)
+        if valid:
+            self.c.setStyleSheet("background: white")
+        else:
+            self.c.setStyleSheet("background: #ff9999")
+        return valid
+
+    def validate_d(self):
+
+        palette = self.d.palette()
+        role = self.d.foregroundRole()
+        palette.setColor(role, Qt.black)
+        self.d.setPalette(palette)
+
+        text = self.d.text()
+        valid = self.validate_polynom(text)
+        if valid:
+            self.d.setStyleSheet("background: white")
+        else:
+            self.d.setStyleSheet("background: #ff9999")
+
+        return valid
 
     def compute(self):
 
