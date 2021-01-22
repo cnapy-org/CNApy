0aadba2b477404d81e9754a88c0dd0f611931e4d
Sven Thiele <sthiele78@gmail.com>
2021-01-22 15:23:29 +0100

    update post-install for windows

--- constructor/post_install.bat
+++ constructor/post_install.bat
@@ -1,2 +1,3 @@
-%~dp0..\Scripts\activate.bat
-pip install cameo==0.13 cobra==0.18.1 qtconsole==4.7.7
+pip install --user cameo==0.13 cobra==0.18.1 qtconsole==4.7.7 --upgrade-strategy only-if-needed --ignore-installed ruamel_yaml
+call %~dp0..\Scripts\activate.bat
+call %~dp0..\Scripts\conda install cnapy -c arb-lab/label/test -c conda-forge -c bioconda --yes
