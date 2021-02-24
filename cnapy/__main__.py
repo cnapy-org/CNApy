af0e716da1a17102a5140a16d97660a0c888ae3b
Sven Thiele <sthiele78@gmail.com>
2021-02-24 11:17:17 +0100

    split engines into separate files

--- cnapy/__main__.py
+++ cnapy/__main__.py
@@ -12,7 +12,6 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-from cnapy.cellnetanalyzer import CellNetAnalyzer
 import configparser
 import os
 import appdirs
@@ -30,6 +29,7 @@ def main_cnapy(args=None):
         if os.path.isfile(oe):
             os.environ['OCTAVE_EXECUTABLE'] = oe
 
+    from cnapy.cellnetanalyzer import CellNetAnalyzer
     CellNetAnalyzer()
 
 
