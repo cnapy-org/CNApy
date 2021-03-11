5415f0e9d8bcf8a32e04f09de3d5d1feb3029d6f
Sven Thiele <sthiele78@gmail.com>
2021-03-11 08:51:58 +0100

    rename to project_downloader

--- constructor/post_install.bat
+++ constructor/post_install.bat
@@ -1,4 +1,4 @@
 call %~dp0..\Scripts\activate.bat
 call %~dp0..\Scripts\conda install cnapy -c arb-lab/label/test -c conda-forge -c bioconda --yes
 pip install --user cameo --upgrade-strategy only-if-needed
-project-downloader
+project_downloader
