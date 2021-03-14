d3bec98f602148f13632eb5c8d7f7a85279d3fef
Sven Thiele <sthiele78@gmail.com>
2021-03-14 11:06:07 +0100

    call conda clean post install

--- constructor/linux/post_install.sh
+++ constructor/linux/post_install.sh
@@ -3,4 +3,5 @@ source $PREFIX/etc/profile.d/conda.sh
 conda activate $PREFIX
 
 conda install -v -c arb-lab/label/test -c conda-forge -c bioconda cnapy --yes
+conda clean --all --yes
 project_downloader
