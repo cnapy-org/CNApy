84b67a31d1039b79191c276428d6095b1ff2ba0b
axelvonkamp <axelk1@gmx.de>
2021-03-18 21:22:29 +0100

    handle elementary modes with the classes in flux_vector_container

--- cnapy/gui_elements/efmtool_dialog.py
+++ cnapy/gui_elements/efmtool_dialog.py
@@ -11,6 +11,7 @@ from cnapy.cnadata import CnaData
 import efmtool_link.efmtool_extern as efmtool_extern
 from cobra.util.array import create_stoichiometric_matrix
 import numpy
+from cnapy.flux_vector_container import FluxVectorMemmap
 
 class EFMtoolDialog(QDialog):
     """A dialog to set up EFM calculation"""
@@ -28,51 +29,6 @@ class EFMtoolDialog(QDialog):
         l1.addWidget(self.constraints)
         self.layout.addItem(l1)
 
-        # l2 = QHBoxLayout()
-        # self.flux_bounds = QGroupBox(
-        #     "use flux bounds to calculate elementary flux vectors")
-        # self.flux_bounds.setCheckable(True)
-        # self.flux_bounds.setChecked(False)
-
-        # vbox = QVBoxLayout()
-        # label = QLabel("Threshold for bounds to be unconstrained")
-        # vbox.addWidget(label)
-        # self.threshold = QLineEdit("100")
-        # validator = QIntValidator()
-        # validator.setBottom(0)
-        # self.threshold.setValidator(validator)
-        # vbox.addWidget(self.threshold)
-        # self.flux_bounds.setLayout(vbox)
-        # l2.addWidget(self.flux_bounds)
-        # self.layout.addItem(l2)
-
-        # l3 = QHBoxLayout()
-        # self.check_reversibility = QCheckBox(
-        #     "check reversibility")
-        # self.check_reversibility.setCheckState(Qt.Checked)
-        # l3.addWidget(self.check_reversibility)
-        # self.layout.addItem(l3)
-
-        # l4 = QHBoxLayout()
-        # self.convex_basis = QCheckBox(
-        #     "only convex basis")
-        # l4.addWidget(self.convex_basis)
-        # self.layout.addItem(l4)
-
-        # l5 = QHBoxLayout()
-        # self.isozymes = QCheckBox(
-        #     "consider isozymes only once")
-        # l5.addWidget(self.isozymes)
-        # self.layout.addItem(l5)
-
-        # # TODO: choose solver
-
-        # l7 = QHBoxLayout()
-        # self.rational_numbers = QCheckBox(
-        #     "use rational numbers")
-        # l7.addWidget(self.rational_numbers)
-        # self.layout.addItem(l7)
-
         lx = QHBoxLayout()
         self.button = QPushButton("Compute")
         self.cancel = QPushButton("Close")
@@ -88,7 +44,7 @@ class EFMtoolDialog(QDialog):
 
     def compute(self):
         stdf = create_stoichiometric_matrix(self.appdata.project.cobra_py_model, array_type='DataFrame')
-        rev = [r.reversibility for r in self.appdata.project.cobra_py_model.reactions]
+        rev = [int(r.reversibility) for r in self.appdata.project.cobra_py_model.reactions]
         scenario = {}
         if self.constraints.checkState() == Qt.Checked:
             for r in self.appdata.project.scen_values.keys():
@@ -97,36 +53,23 @@ class EFMtoolDialog(QDialog):
                     del rev[stdf.columns.get_loc(r)]
                     del stdf[r] # delete the column with this reaction id from the data frame
                     scenario[r] = (0, 0)
-        ems = efmtool_extern.calculate_flux_modes(stdf.values, numpy.array(rev, dtype=int))
-        idx = stdf.columns.tolist()
+        work_dir = efmtool_extern.calculate_flux_modes(stdf.values, rev, return_work_dir_only=True)
+        reac_id = stdf.columns.tolist()
 
-        self.result2ui(ems, idx, scenario)
+        self.result2ui(work_dir, reac_id, scenario)
 
-    def result2ui(self, ems, idx, scenario):
-        if ems is None:
+    def result2ui(self, work_dir, reac_id, scenario):
+        if work_dir is None:
             QMessageBox.information(self, 'No modes',
-                                    'An error occured and modes have not been calculated.')
-        elif ems.shape[1] == 0:
-            QMessageBox.information(self, 'No modes',
-                                    'No elementary modes exist.')
+                                    'An error occured and modes have not been calculated.')            
         else:
-            oems = [None] * ems.shape[1]
-            for j in range(ems.shape[1]):
-                oems[j] = {idx[i]: float(ems[i, j]) for i in range(ems.shape[0]) if ems[i, j] != 0}
-                # print("Mode:")
-                # count_ccc = 0
-                # omode = {}
-                # for element in mode:
-                #     idx2 = int(idx[0][count_ccc])-1
-                #     reaction = reac_id[idx2]
-                #     print("element: ", count_ccc,
-                #           idx2, reaction, element)
-                #     count_ccc += 1
-                #     if element != 0:
-                #         omode[reaction] = float(element)
-                # oems.append(omode)
-            self.appdata.project.modes = oems
-            self.centralwidget.mode_navigator.current = 0
-            self.centralwidget.mode_navigator.scenario = scenario
-            self.centralwidget.mode_navigator.title.setText("Mode Navigation")
-            self.centralwidget.update_mode()
+            ems = FluxVectorMemmap('efms.bin', reac_id, containing_temp_dir=work_dir)
+            if len(ems) == 0:
+                QMessageBox.information(self, 'No modes',
+                                        'No elementary modes exist.')
+            else:
+                self.appdata.project.modes = ems
+                self.centralwidget.mode_navigator.current = 0
+                self.centralwidget.mode_navigator.scenario = scenario
+                self.centralwidget.mode_navigator.title.setText("Mode Navigation")
+                self.centralwidget.update_mode()
