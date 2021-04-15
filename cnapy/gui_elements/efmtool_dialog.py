2bcb32f13a428bf689e4ebd1ce6664d9a7681293
Sven Thiele <sthiele78@gmail.com>
2021-04-15 08:37:35 +0200

    formating

--- cnapy/gui_elements/efmtool_dialog.py
+++ cnapy/gui_elements/efmtool_dialog.py
@@ -1,18 +1,15 @@
 """The cnapy elementary flux modes calculator dialog"""
-import io
-import traceback
-
+import efmtool_link.efmtool4cobra as efmtool4cobra
+import efmtool_link.efmtool_extern as efmtool_extern
+import numpy
+from cobra.util.array import create_stoichiometric_matrix
 from qtpy.QtCore import Qt
-from qtpy.QtGui import QIntValidator
-from qtpy.QtWidgets import (QCheckBox, QDialog, QGroupBox, QHBoxLayout, QLabel,
-                            QLineEdit, QMessageBox, QPushButton, QVBoxLayout)
+from qtpy.QtWidgets import (QCheckBox, QDialog, QHBoxLayout, QMessageBox,
+                            QPushButton, QVBoxLayout)
 
 from cnapy.cnadata import CnaData
-import efmtool_link.efmtool_extern as efmtool_extern
-import efmtool_link.efmtool4cobra as efmtool4cobra
-from cobra.util.array import create_stoichiometric_matrix
-import numpy
 from cnapy.flux_vector_container import FluxVectorMemmap
+
 
 class EFMtoolDialog(QDialog):
     """A dialog to set up EFM calculation"""
@@ -21,7 +18,7 @@ class EFMtoolDialog(QDialog):
         QDialog.__init__(self)
         self.appdata = appdata
         self.centralwidget = centralwidget
-    
+
         self.layout = QVBoxLayout()
 
         l1 = QHBoxLayout()
@@ -44,8 +41,10 @@ class EFMtoolDialog(QDialog):
         self.button.clicked.connect(self.compute)
 
     def compute(self):
-        stdf = create_stoichiometric_matrix(self.appdata.project.cobra_py_model, array_type='DataFrame')
-        reversible, irrev_backwards_idx = efmtool4cobra.get_reversibility(self.appdata.project.cobra_py_model)
+        stdf = create_stoichiometric_matrix(
+            self.appdata.project.cobra_py_model, array_type='DataFrame')
+        reversible, irrev_backwards_idx = efmtool4cobra.get_reversibility(
+            self.appdata.project.cobra_py_model)
         if len(irrev_backwards_idx) > 0:
             irrev_back = numpy.zeros(len(reversible), dtype=numpy.bool)
             irrev_back[irrev_backwards_idx] = True
@@ -56,14 +55,16 @@ class EFMtoolDialog(QDialog):
                 if vl == vu and vl == 0:
                     r_idx = stdf.columns.get_loc(r)
                     del reversible[r_idx]
-                    del stdf[r] # delete the column with this reaction id from the data frame
+                    # delete the column with this reaction id from the data frame
+                    del stdf[r]
                     if len(irrev_backwards_idx) > 0:
                         irrev_back = numpy.delete(irrev_back, r_idx)
                     scenario[r] = (0, 0)
         if len(irrev_backwards_idx) > 0:
             irrev_backwards_idx = numpy.where(irrev_back)[0]
             stdf.values[:, irrev_backwards_idx] *= -1
-        work_dir = efmtool_extern.calculate_flux_modes(stdf.values, reversible, return_work_dir_only=True)
+        work_dir = efmtool_extern.calculate_flux_modes(
+            stdf.values, reversible, return_work_dir_only=True)
         reac_id = stdf.columns.tolist()
 
         self.result2ui(work_dir, reac_id, scenario, irrev_backwards_idx)
@@ -71,9 +72,10 @@ class EFMtoolDialog(QDialog):
     def result2ui(self, work_dir, reac_id, scenario, irrev_backwards_idx):
         if work_dir is None:
             QMessageBox.information(self, 'No modes',
-                                    'An error occured and modes have not been calculated.')            
+                                    'An error occured and modes have not been calculated.')
         else:
-            ems = FluxVectorMemmap('efms.bin', reac_id, containing_temp_dir=work_dir)
+            ems = FluxVectorMemmap('efms.bin', reac_id,
+                                   containing_temp_dir=work_dir)
             if len(ems) == 0:
                 QMessageBox.information(self, 'No modes',
                                         'No elementary modes exist.')
@@ -82,5 +84,6 @@ class EFMtoolDialog(QDialog):
                 self.appdata.project.modes = ems
                 self.centralwidget.mode_navigator.current = 0
                 self.centralwidget.mode_navigator.scenario = scenario
-                self.centralwidget.mode_navigator.title.setText("Mode Navigation")
+                self.centralwidget.mode_navigator.title.setText(
+                    "Mode Navigation")
                 self.centralwidget.update_mode()
