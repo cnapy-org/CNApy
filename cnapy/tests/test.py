067e4e64effe1dc1e95e069b7247a250e185840a
Sven Thiele <sthiele78@gmail.com>
2021-04-15 09:28:18 +0200

    add test for get_reversibility()

--- cnapy/tests/test.py
+++ cnapy/tests/test.py
@@ -1,2 +1,11 @@
+import cobra
+import efmtool_link.efmtool4cobra as efmtool4cobra
+
+
 def test_import():
     import cnapy
+
+
+def test_efmtool4cobra_get_reversibility():
+    model = cobra.Model()
+    reversible, irrev_backwards_idx = efmtool4cobra.get_reversibility(model)
