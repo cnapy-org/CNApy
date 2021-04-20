e4b74a31e8dd79727f45e4dc88ed7dff2ba4b638
axelvonkamp <axelk1@gmx.de>
2021-04-20 14:42:58 +0200

    do flipping back of irreversible backwards reactions in the core function use correct function for deleting elements from the numpy array "reversible"

--- cnapy/gui_elements/efmtool_dialog.py
+++ cnapy/gui_elements/efmtool_dialog.py
@@ -5,7 +5,6 @@ from qtpy.QtWidgets import (QCheckBox, QDialog, QHBoxLayout, QMessageBox,
 
 import cnapy.core
 from cnapy.cnadata import CnaData
-from cnapy.flux_vector_container import FluxVectorMemmap
 
 
 class EFMtoolDialog(QDialog):
@@ -38,21 +37,19 @@ class EFMtoolDialog(QDialog):
         self.button.clicked.connect(self.compute)
 
     def compute(self):
-        (work_dir, reac_id, scenario, irrev_backwards_idx) = cnapy.core.efm_computation(
+        (ems, scenario) = cnapy.core.efm_computation(
             self.appdata.project.cobra_py_model, self.appdata.project.scen_values,
             self.constraints.checkState() == Qt.Checked)
 
-        if work_dir is None:
+        if ems is None:
             QMessageBox.information(self, 'No modes',
                                     'An error occured and modes have not been calculated.')
         else:
-            ems = FluxVectorMemmap('efms.bin', reac_id,
-                                   containing_temp_dir=work_dir)
             if len(ems) == 0:
                 QMessageBox.information(self, 'No modes',
                                         'No elementary modes exist.')
             else:
-                ems.fv_mat[:, irrev_backwards_idx] *= -1
+                print(scenario)
                 self.appdata.project.modes = ems
                 self.centralwidget.mode_navigator.current = 0
                 self.centralwidget.mode_navigator.scenario = scenario
