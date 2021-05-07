73ed3b24abe7b62d779181d4a3358cc604408247
Sven Thiele <sthiele78@gmail.com>
2021-05-07 10:33:53 +0200

    fix constructor

--- constructor/win/post_install.bat
+++ constructor/win/post_install.bat
@@ -1,4 +1,10 @@
 call %~dp0..\Scripts\activate.bat
-call %~dp0..\Scripts\conda install cnapy -c cnapy -c conda-forge --yes
+@REM @REM call %~dp0..\Scripts\conda config --set unsatisfiable_hints True
+@REM call %~dp0..\Scripts\conda config --set remote_read_timeout_secs 120
+@REM call %~dp0..\Scripts\conda config --set remote_connect_timeout_secs: 60
+@REM call mamba install optlang_enumerator -c cnapy -c conda-forge -v --yes
+@REM @REM call pause
+@REM call mamba install cnapy -c cnapy -c conda-forge -v --yes
+@REM call pause
 call %~dp0..\Scripts\conda clean --all --yes
 project_downloader
