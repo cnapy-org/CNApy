433c72147954188d2d335162024fee79c863b9e0
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2021-08-30 15:40:46 +0200

    Set version number 1.0.3

--- cnapy/appdata.py
+++ cnapy/appdata.py
@@ -15,7 +15,7 @@ class AppData:
     ''' The application data '''
 
     def __init__(self):
-        self.version = "cnapy-1.0.2"
+        self.version = "cnapy-1.0.3"
         self.format_version = 1
         self.unsaved = False
         self.project = ProjectData()
