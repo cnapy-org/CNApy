f975e1ff64b94e5b31d0513ad98ff3bf1f061a39
axelvonkamp <axelk1@gmx.de>
2022-07-29 16:13:08 +0200

    resolve IDs beginning with "R_" in scenarios

--- cnapy/appdata.py
+++ cnapy/appdata.py
@@ -187,6 +187,14 @@ class Scenario(Dict[str, Tuple[float, float]]):
         scen_values = []
         for reac_id, val in flux_values.items():
             if reac_id in appdata.project.cobra_py_model.reactions:
+                found_reac_id = True
+            elif reac_id.startswith("R_"):
+                reac_id = reac_id[2:]
+                if reac_id in appdata.project.cobra_py_model.reactions:
+                    found_reac_id = True
+                else:
+                    found_reac_id = False
+            if found_reac_id:
                 reactions.append(reac_id)
                 scen_values.append(val)
             else:
