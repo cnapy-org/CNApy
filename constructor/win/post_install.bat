2f5fd1666a87ad3bfcae3a34d61be49a27ae8799
Pavlos Stephanos Bekiaris <36934614+Paulocracy@users.noreply.github.com>
2023-01-24 15:02:01 +0100

    Bump to 1.1.5 (#452)

--- constructor/win/post_install.bat
+++ constructor/win/post_install.bat
@@ -1,5 +1,5 @@
 call %~dp0..\Scripts\activate.bat
 
-call mamba install cnapy=1.1.4 -c cnapy -c conda-forge -v --yes
+call mamba install cnapy=1.1.5 -c cnapy -c conda-forge -v --yes
 call pause
 call %~dp0..\Scripts\conda clean --all --yes
